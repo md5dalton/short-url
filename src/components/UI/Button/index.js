33d905cea3ca62ac8ba082b6f6bfc097f9ba2c5f
md5dalton <md5daltonggs@gmail.com>
2022-08-02 22:30:02 -0400

    Update index.js

--- src/components/UI/Button/index.js
+++ src/components/UI/Button/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 import "./styles.sass"
 
-export default ({ role, type, className, id, onClick, children }) => (
+// export default ({ role, type, className, id, onClick, children }) => (
+export default ({ className, role, children, ...rest }) => (
     <button
-        id={id}
         className={`${role ? role : "primary"}${className ? " " + className : ""}`}
-        type={type}
-        onClick={onClick}
+        {...rest}
     >
         {children}
     </button>
